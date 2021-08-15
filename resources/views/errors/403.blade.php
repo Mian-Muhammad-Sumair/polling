1ae48d551e3c3d84962b348f2de6443fbbe3fba6
Sumair <sumair8683@gmail.com>
2021-08-15 20:31:02 +0500

    Add Exception pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title')
+403 Unauthorized Page
+@endsection
+@section('content')
+
+<div class="container">
+    <div class="error-msg-screen">
+       <img src="https://i.pinimg.com/originals/91/22/07/912207f3b7e2120c417d73ebc411f949.png" alt="">
+        <h5>Unauthorized Page.</h5>
+    </div>
+</div>
+@endsection
+
+@section('extra_js')
+
+@endsection
