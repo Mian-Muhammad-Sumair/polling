9dcd1d93d4120b522dae7eb2e55e6a578b773c45
Sumair <sumair8683@gmail.com>
2021-08-16 11:10:09 +0500

    Table Responsive

--- resources/views/admin/index_table.blade.php
+++ resources/views/admin/index_table.blade.php
@@ -7,7 +7,9 @@
 @section('content')
     <div class="container login-container content-order animatedParent">
         <div class="table-outer-body">
+            <div class="table table-responsive">
             {!! $dataTable->table() !!}
+            </div>
         </div>
     </div>
     <style>
