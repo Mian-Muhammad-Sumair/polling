3bf33d5c851b2db0993460bc5b53f14304e72ab7
Haider Adrees <haider@geniusmindzone.com>
2021-08-18 21:56:35 +0500

    design fixing

--- resources/views/poll/pollView.blade.php
+++ resources/views/poll/pollView.blade.php
@@ -1,77 +1,72 @@
 @extends('layouts.app')
 @section('title')
-    Poll Data
+Poll Data
 @endsection
 @section('content')
-    <div class="container">
-        <div class="dashboard-body">
-            <div class="top-header">
-                <div>
-                    <h2> <b>Poll Data</b></h2>
-                    <div class="underline"></div>
-                </div>
+<div class="container">
+    <div class="dashboard-body">
+        <div class="top-header">
+            <div>
+                <h2> <b>Poll Data</b></h2>
+                <div class="underline"></div>
             </div>
-            <div class=" ">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <label><b>Name:</b></label><label> {{$poll->name}}</label>
-                    </div>
-                    <div class="col-lg-4">
-                        <label><b>Info:</b></label><label> {{$poll->info}}</label>
-                    </div>
-                    <div class="col-lg-4">
-                        <label><b>Question:</b></label><label> {{$poll->question}}</label>
-                    </div>
-                    <div class="col-lg-6">
-                        <label><b>Poll Key:</b></label><br>
-                        @foreach($poll->pollkeys as $key)
-                            <label>000 {{$key->key}}</label><br>
-                            <label> {{$key->key}}</label><br>
-                        @endforeach
-                    </div>
-                    <div class="col-lg-12">
-                        <label><b>Poll identifier Question:</b></label>
-                        <table>
-                            <th>
-                            <td>Question</td>
-                            </th>
+        </div>
+        <div class="poll-date-section">
+            <div class="row">
+                <div class="col-lg-6">
+                    <label><b>Name :</b></label><label> {{$poll->name}}</label>
+                </div>
+                <div class="col-lg-6">
+                    <label><b>Info :</b></label><label> {{$poll->info}}</label>
+                </div>
+                <div class="col-lg-6">
+                    <label><b>Question :</b></label><label> {{$poll->question}}</label>
+                </div>
+                <div class="col-lg-6">
+                    <label><b>Poll Key :</b></label>
+                    @foreach($poll->pollkeys as $key)
+                    <label> {{$key->key}}</label>
+                    <label> {{$key->key}}</label><br>
+                    @endforeach
+                </div>
+                <div class="col-lg-12">
+                    <label><b>Poll identifier Question :</b></label>
+                    <ol>
                         @foreach($poll->pollIdentifierQuestions as $question)
-                                <tr><td> {{$question->identifier_question}}</td></tr>
+
+                        <li> {{$question->identifier_question}}</li>
+
                         @endforeach
-                        </table>
-                    </div>
-
-
+                    </ol>
                 </div>
             </div>
         </div>
     </div>
-    <div class="container login-container content-order animatedParent">
-        <div class="table-outer-body">
-            {!! $dataTable->table() !!}
-        </div>
+</div>
+<div class="container login-container content-order animatedParent">
+    <div class="table-outer-body">
+        {!! $dataTable->table() !!}
     </div>
-    <style>
-        .dataTables_wrapper .dataTables_paginate .paginate_button.disabled, z
-        .dataTables_wrapper .dataTables_paginate .paginate_button.disabled:hover,
-        .dataTables_wrapper .dataTables_paginate .paginate_button.disabled:active,
-        .dataTables_wrapper .dataTables_length,
-        .dataTables_wrapper .dataTables_filter,
-        .dataTables_wrapper .dataTables_info,
-        .dataTables_wrapper .dataTables_processing,
-        .dataTables_wrapper .dataTables_paginate {
-            color: #3b83f6b5  !important;
-        }
-    </style>
+</div>
+<style>
+    .dataTables_wrapper .dataTables_paginate .paginate_button.disabled,
+    z .dataTables_wrapper .dataTables_paginate .paginate_button.disabled:hover,
+    .dataTables_wrapper .dataTables_paginate .paginate_button.disabled:active,
+    .dataTables_wrapper .dataTables_length,
+    .dataTables_wrapper .dataTables_filter,
+    .dataTables_wrapper .dataTables_info,
+    .dataTables_wrapper .dataTables_processing,
+    .dataTables_wrapper .dataTables_paginate {
+        color: #3b83f6b5 !important;
+    }
+</style>
 
 @endsection
 
 @section('extra_js')
-    {!! $dataTable->scripts() !!}
+{!! $dataTable->scripts() !!}
 @endsection
 
 @section('extra_css')
 
 @endsection
-
-
