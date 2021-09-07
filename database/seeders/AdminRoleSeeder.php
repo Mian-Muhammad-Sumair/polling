4772070627a3cbc3d86261ba225da76be804dd46
Sumair <sumair8683@gmail.com>
2021-09-07 19:21:56 +0500

    update issue on permission

--- database/seeders/AdminRoleSeeder.php
+++ database/seeders/AdminRoleSeeder.php
@@ -36,11 +36,11 @@ class AdminRoleSeeder extends Seeder
         ];
         $defaultPermissionsNames=[];
         foreach ($roles as $role => $permissionsNames) {
-            if($role=='Owner' || $role=='Administrator'){
-                $permissions = Permission::where('guard_name', 'employee')->get()->pluck('id'); // get guard name employee all permissions ids
+            if($role=='Admin'){
+                $permissions = Permission::where('guard_name', 'admin')->get()->pluck('id'); // get guard name employee all permissions ids
             }else{
                 $permissionsNames=array_merge($permissionsNames,$defaultPermissionsNames);
-                $permissions = Permission::whereIn('name', $permissionsNames)->where('guard_name', 'employee')->get()->pluck('id'); // get guard name employee custom permissions ids
+                $permissions = Permission::whereIn('name', $permissionsNames)->where('guard_name', 'admin')->get()->pluck('id'); // get guard name employee custom permissions ids
             }
             $this->roleRepository->create([
                 'name' => $role,
