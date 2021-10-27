240ec73465130027bf37a99f214e10f156b2c670
hassanmehar266 <58462570+hassanmehar266@users.noreply.github.com>
2021-10-27 09:12:20 +0500

    change auto gernate poll key

--- resources/views/poll/create.blade.php
+++ resources/views/poll/create.blade.php
@@ -331,8 +331,8 @@ Create Poll
 
     function makekey() {
         var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for (var i = 0; i < 20; i++)
+        var possible = "0123456789";
+        for (var i = 0; i < 10; i++)
             text += possible.charAt(Math.floor(Math.random() * possible.length));
         return text;
     }
--- resources/views/poll/edit.blade.php
+++ resources/views/poll/edit.blade.php
@@ -367,9 +367,9 @@
 
         function makekey() {
             var text = "";
-            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var possible = "0123456789";
 
-            for (var i = 0; i < 20; i++)
+            for (var i = 0; i < 10; i++)
                 text += possible.charAt(Math.floor(Math.random() * possible.length));
 
             return text;
